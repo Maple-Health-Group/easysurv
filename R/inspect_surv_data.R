ff2423d0441f1dd65cb8c76d13c6635c2af897f8
Niall Davison <niall.davison@maplehealthgroup.com>
2024-06-14 11:25:58 +0100

    Improve printing for inspect_surv_data

--- R/inspect_surv_data.R
+++ R/inspect_surv_data.R
@@ -12,6 +12,7 @@
 #' @importFrom purrr discard
 #' @importFrom stats as.formula
 #' @importFrom survival Surv survfit
+#' @importFrom tibble as_tibble
 #'
 #' @examples
 #'
@@ -22,10 +23,9 @@
 #'   group = "group"
 #' )
 #'
-#'
 inspect_surv_data <- function(data, time, event, group = NULL) {
   # Create visible binding for R CMD check.
-  n <- factor_warning <- NULL
+  n <- group_is_factor <- NULL
 
   ## Check data ----
   # Is it a data frame?
@@ -69,11 +69,7 @@ inspect_surv_data <- function(data, time, event, group = NULL) {
   } else {
     if (!is.factor(data[[group]])) {
       data[[group]] <- as.factor(data[[group]])
-      factor_warning <- paste0(
-        "The group column is not a factor variable. ",
-        "It is advised to convert this to a factor for ",
-        "other easysurv functions."
-      )
+      group_is_factor <- FALSE
     }
 
     sample_sizes <- data |> dplyr::count(!!as.name(group))
@@ -87,11 +83,39 @@ inspect_surv_data <- function(data, time, event, group = NULL) {
     first_few_rows = first_few_rows,
     sample_sizes = sample_sizes,
     events_summary = events_summary,
-    survival_summary = survival_summary,
-    factor_warning = factor_warning
+    survival_summary = survival_summary
   )
+
+  out <- lapply(out, tibble::as_tibble)
+
+  out <- c(out, group_is_factor = group_is_factor)
 
   out <- out |> purrr::discard(is.null)
 
+  class(out) <- c(class(out), "inspect_surv_data")
+
   out
+}
+
+#' Print methods for \code{inspect_surv_data}
+#' @param x An object of class \code{inspect_surv_data}
+#' @param ... Additional arguments
+#' @export
+#' @noRd
+#' @importFrom cli cli_alert_warning
+print.inspect_surv_data <- function(x, ...) {
+  # Show all columns in each tibble.
+  lapply(x, function(element) {
+    if (inherits(element, "tbl_df")) {
+      print(element, width = Inf, ...)
+    }
+  })
+
+  if (!is.null(x$group_is_factor) && !x$group_is_factor) {
+    cli::cli_alert_warning(c(
+      "{.field group} column is not a factor variable. \n",
+      "It is recommended to convert it to a factor for other easysurv ",
+      "functions."
+    ))
+  }
 }
