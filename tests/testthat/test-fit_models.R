00716697974d342749d8baa973d09e9202df47ef
Niall Davison <niall.davison@maplehealthgroup.com>
2024-06-18 13:16:06 +0100

    fit_models now forces predict_by into a factor

--- tests/testthat/test-fit_models.R
+++ tests/testthat/test-fit_models.R
@@ -1,3 +1,14 @@
+test_that("models work without predict_by", {
+  expect_no_error(
+    fit_models(
+      data = easy_lung,
+      time = "time",
+      event = "status",
+      engine = "flexsurv"
+    )
+  )
+})
+
 test_that("separate models work", {
   expect_no_error(
     fit_models(
@@ -11,13 +22,9 @@ test_that("separate models work", {
 })
 
 test_that("joint models work", {
-  # predict_by should use a factor variable
-  test_data <- easy_lung
-  test_data$sex <- as.factor(test_data$sex)
-
   expect_no_error(
     fit_models(
-      data = test_data,
+      data = easy_lung,
       time = "time",
       event = "status",
       predict_by = "sex",
@@ -73,12 +80,9 @@ test_that("separate models work with survival engine", {
 })
 
 test_that("joint models work with survival engine", {
-  test_data <- easy_lung
-  test_data$sex <- as.factor(test_data$sex)
-
   expect_no_error(
     fit_models(
-      data = test_data,
+      data = easy_lung,
       time = "time",
       event = "status",
       predict_by = "sex",
